f4480f79a006009207bc4b1109d47c48f875a2fa
chandanverma03 <114793891+chandanverma03@users.noreply.github.com>
2022-12-28 21:58:31 +0530

    Add files via upload

---
+++ tuple.py
@@ -0,0 +1,25 @@
+# tuple data structure 
+# tuple can store any data type 
+# most important tuples are immutable, once tuple created we can't change 
+# data inside tuple
+
+# no append, no insert , no pop , no remove
+# days = ('monday', 'tuesday')
+# tuple are faster than lists 
+
+# methods 
+# count, index
+# len funtion
+# slicing 
+
+# mixed = (1,2,4,5,6)
+# for i in mixed:
+#     print(i)
+
+
+#single tuple can be written in (1,) this form 
+
+#tuple unpacking 
+fruits = 'apple','mango','banana'
+a, b, c = (fruits)
+print(a)
